7b0566a91c1b1464dec0c15cd9febfbdd8d915d3
chanakeyan <chanakeyan433@gmail.com>
2026-07-10 09:53:49 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/legal/lens/config/SecurityConfig.java
+++ src/main/java/com/example/legal/lens/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
 				"http://localhost:5500",
 				"http://127.0.0.1:5500",
 				"http://localhost:5173",
-				"http://127.0.0.1:5173"));
+				"http://127.0.0.1:5173",
+		         "https://chanakeyan.github.io/"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 
